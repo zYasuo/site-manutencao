c58599ac9a77d2a087757ddc49d7c0bb5b4632a9
Danilo Silva <dansilvaagr@hotmail.com>
2024-04-23 20:17:56 -0300

    Site Manutençao NR

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+     remotePatterns: [
+        {
+           protocol: 'https',
+           hostname: 'avatars.githubusercontent.com',
+           pathname: '**',
+        },
+        {
+           protocol: 'https',
+           hostname: 'www.nrconexoes.com.br',
+           pathname: '**',
+        },
+        {
+           protocol: 'https',
+           hostname: 'placehold.co',
+           pathname: '**',
+        },
+        {
+         protocol: 'https',
+         hostname: 'cdnimgs.nrconexoes.com.br',
+      }
+     ],
+  },
+}
 
 export default nextConfig;
